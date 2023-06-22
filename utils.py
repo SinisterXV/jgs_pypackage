0ab23186b64e0d0464c23e333b3a108c0c777524
Giuseppe Silvestri s307792 <s307792@studenti.polito.it>
2023-06-22 19:50:35 +0200

    Initial commit

---
+++ utils.py
@@ -0,0 +1,31 @@
+import random
+import numpy as np
+
+rng = random.SystemRandom()
+
+def list2bin(list: list[int] = [], width: int = 32) -> list[str]:
+    """This function returns the binary representation of `list` on `width` bits """
+    binList = []
+    
+    for num in list:
+        binList.append(np.binary_repr(num, width=width))  
+         
+    return binList
+
+
+def randomInts(signed: bool = False, amount: int = 10, width: int = 32) -> list[int]:
+    """This function returns a list of `amount` integers, signed or unsigned, representable with `width` bits """
+    intList = []
+    
+    if (signed):
+        lowerBound = -2**(width - 1)
+        upperBound = 2**(width - 1) - 1
+    else:
+        lowerBound = 0
+        upperBound = 2**width - 1
+        
+    for i in range(0, amount):
+        intList.append(round(rng.uniform(lowerBound, upperBound)))
+        
+    return intList
+    
